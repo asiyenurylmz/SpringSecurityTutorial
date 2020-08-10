9b026e435482f2902bb36c61ef003366a3492f73
Asiyenur Yılmaz <asiyenurylmz17@gmail.com>
2020-08-11 00:15:13 +0300

    first commit

---
+++ src/main/java/securingweb/SpringSecurityTutorialApplication.java
@@ -0,0 +1,13 @@
+package securingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTutorialApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SpringSecurityTutorialApplication.class, args);
+	}
+
+}
